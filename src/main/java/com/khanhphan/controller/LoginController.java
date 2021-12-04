8469a6f2e16dc96731671676731217fa2ddf38fc
khangtan31 <85958587+khangtan31@users.noreply.github.com>
2021-12-04 17:50:02 +0700

    commit login

--- src/main/java/com/khanhphan/controller/LoginController.java
+++ src/main/java/com/khanhphan/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.khanhphan.controller;
 
 import com.khanhphan.data.UserDao;
+import com.khanhphan.model.User;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,10 +22,57 @@ public class LoginController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
         dispatcher.forward(req, resp);
+        doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+
+        String userName = req.getParameter("user-name");
+        String password = req.getParameter("user-password");
+        User user = new User();
+
+        if (userName.isEmpty()) {
+            req.setAttribute("errUsername", "Please input field username!");
+            req.setAttribute("classErrUsername", "errInput");
+        } else {
+            req.setAttribute("txtUsername", userName);
+        }
+
+        if (password.isEmpty()) {
+            req.setAttribute("errPassword", "Please input field password!");
+            req.setAttribute("classErrPassword", "errInput");
+        } else {
+            req.setAttribute("txtPassword", password);
+        }
+
+        if (!userName.isEmpty() && !password.isEmpty()) {
+            user = userDao.findUserByEmail(userName);
+            if (user == null) {
+                req.setAttribute("errUsername", "Username not fould. Please input new username!");
+                req.setAttribute("classErrUsername", "errInput");
+                req.setAttribute("errPassword", "Password is invalid. Please input new password!");
+                req.setAttribute("classErrPassword", "errInput");
+            } else {
+                req.setAttribute("txtUsername", userName);
+                req.setAttribute("classErrUsername", " ");
+                if (!password.equals(user.getPassword())) {
+                    req.setAttribute("errPassword", "Password is invalid. Please input new password!");
+                    req.setAttribute("classErrPassword", "errInput");
+                } else {
+                    req.getSession().setAttribute("Account", user);
+                    resp.sendRedirect("/demo/demo2");
+                }
+            }
+        }
+        try {
+            req.getRequestDispatcher("/views/login.jsp").forward(req, resp);
+        } catch (Exception e) {
+
+        }
+
+        doGet(req, resp);
     }
 }
--- src/main/java/com/khanhphan/model/User.java
+++ src/main/java/com/khanhphan/model/User.java
@@ -20,4 +20,36 @@ public class User {
     private String password;
     @Column(name="code")
     private String code;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
