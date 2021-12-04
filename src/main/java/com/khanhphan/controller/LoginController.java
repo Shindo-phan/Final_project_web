f3563678a3bc1709d38c50b9ee5bfb048bd20460
khangtan31 <85958587+khangtan31@users.noreply.github.com>
2021-12-04 16:20:34 +0700

    commit login

---
+++ src/main/java/com/khanhphan/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.khanhphan.controller;
+
+import com.khanhphan.data.UserDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginController extends HttpServlet {
+
+    private UserDao userDao;
+
+    public LoginController() {
+        userDao = new UserDao();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
